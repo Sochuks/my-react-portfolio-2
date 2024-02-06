e48d29f9c11f3932671a40ba846f50a2b9fb7670
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-02-06 04:31:28 +0100

    Craete top-bar and add links. Modify CSS

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,29 @@
 import Image from "next/image";
+import {BsFillMoonStarsFill} from "react-icons/bs";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>MY REACT PORTFOLIO PAGE</h2>
+    <main className="bg-white px-10">
+      
+      <section className="min-h-screen">
+
+          <div className="py-10 mb-12 flex justify-between">
+              <h1>DevelopedBySochuks</h1>
+              
+              <ul className="flex px-8 justify-center">
+                  <li> 
+                    <BsFillMoonStarsFill /> 
+                  </li>
+
+                  <li>
+                    <a href="#">Resume</a>
+                  </li>
+              </ul>
+
+          </div>
+
+      </section>
+
     </main>
   );
 }

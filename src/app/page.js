824cdcc5ed7b30ec33c52336258c1c65c93d3db8
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-02-06 04:38:09 +0100

    Modify style in Top-bar links

--- src/app/page.js
+++ src/app/page.js
@@ -8,15 +8,15 @@ export default function Home() {
       <section className="min-h-screen">
 
           <div className="py-10 mb-12 flex justify-between">
-              <h1>DevelopedBySochuks</h1>
+              <h1 className="text-xl">DevelopedBySochuks</h1>
               
-              <ul className="flex px-8 justify-center">
+              <ul className="flex px-8 items-center">
                   <li> 
-                    <BsFillMoonStarsFill /> 
+                    <BsFillMoonStarsFill className="cursor-pointer text-xl" /> 
                   </li>
 
                   <li>
-                    <a href="#">Resume</a>
+                    <a className="bg-gradient-to-r from-cyan-400 to-teal-500 px-4 py-2 rounded-md ml-8" href="#">Resume</a>
                   </li>
               </ul>
 

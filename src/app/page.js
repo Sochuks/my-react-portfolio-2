f57c1c27b769cc7ba4e981d0a5bd18b8ad7522ae
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-02-06 05:22:57 +0100

    Modify Intro-text styles

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
     <main className="bg-white px-10">
       
       <section className="min-h-screen">
-          /* Top-Bar */
+          {/* Top-Bar */}
           <div className="py-10 mb-12 flex justify-between">
               <h1 className="text-xl">DevelopedBySochuks</h1>
               
@@ -25,10 +25,10 @@ export default function Home() {
           </div>
 
           {/* Intro-Text --*/}
-          <div>
-              <h2>Moses Sochukwuma Kajang</h2>
-              <h3>Software Engineer - Web Developer</h3>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores rerum vel vero quaerat atque obcaecati libero aperiam velit recusandae repellat.</p>
+          <div className="text-center p-10">
+              <h2 className="text-5xl text-teal-400 py-2 font-medium">Moses Sochukwuma Kajang</h2>
+              <h3 className="text-2xl py-2">Software Engineer - Web Developer</h3>
+              <p className="text-md py-5 leading-8 text-gray-800">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores rerum vel vero quaerat atque obcaecati libero aperiam velit recusandae repellat.</p>
           </div>
 
           {/*-- Social-Icons --*/}

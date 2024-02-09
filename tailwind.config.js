82834d500493e27998b64e1b26493e34c7a14333
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-02-09 15:02:37 +0100

    Create global styling variables for css & tailwind including(screen, colors etc.)

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,24 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    // Screen Sizes
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
     extend: {
+      // Portfolio Color Scheme
+      colors:{
+        bgColor: '#1f1f38',
+        bg2Color: '#2c2c6c',
+        primaryColor: '4db5ff',
+        primary2Color: 'rgb(77, 181, 125, 0.4)',
+        whiteColor: '#fff',
+        lightColor: 'rgb(255, 255, 255, 0.6)',
+      },
+
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
